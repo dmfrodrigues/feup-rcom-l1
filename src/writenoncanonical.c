61bf90ebcd437909e4743cbba0cd26e04cba5148
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2020-09-30 20:27:46 +0100

    Reindented and added headers

--- src/writenoncanonical.c
+++ src/writenoncanonical.c
@@ -1,49 +1,50 @@
 /*Non-Canonical Input Processing*/
 
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <fcntl.h>
-#include <termios.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <termios.h>
+#include <unistd.h>
 
 #define BAUDRATE B38400
-#define MODEMDEVICE "/dev/ttyS1"
+// #define MODEMDEVICE "/dev/ttyS1"
 #define _POSIX_SOURCE 1 /* POSIX compliant source */
 #define FALSE 0
 #define TRUE 1
 
-volatile int STOP=FALSE;
+// volatile int STOP = FALSE;
 
-int main(int argc, char** argv)  /* fazer write da serial port */
-{
-    int fd,c, res;
-    struct termios oldtio,newtio;
-    char buf[255];   /* linha do stdin que vamos ler? */
+int main(int argc, char** argv){ /* fazer write da serial port */
+    int fd, c, res;
+    struct termios oldtio, newtio;
+    char buf[255]; /* linha do stdin que vamos ler? */
     int i, sum = 0, speed = 0;
-    
-    if ( (argc < 2) || 
-  	     ((strcmp("/dev/ttyS0", argv[1])!=0) && 
-  	      (strcmp("/dev/ttyS1", argv[1])!=0) )) {
-      printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
-      exit(1);
+
+    if(argc < 2){
+        printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
+        exit(1);
     }
 
-
-  /*
+    /*
     Open serial port device for reading and writing and not as controlling tty
     because we don't want to get killed if linenoise sends CTRL-C.
-  */
+     */
 
+    fd = open(argv[1], O_RDWR | O_NOCTTY); /* serial port */
+    if(fd < 0) {
+        perror(argv[1]);
+        exit(-1);
+    } /* erro na leitura da porta */
 
-    fd = open(argv[1], O_RDWR | O_NOCTTY );/* porta s�rie  */
-    if (fd <0) {perror(argv[1]); exit(-1); } /* erro na leitura da porta */
-
-    if ( tcgetattr(fd,&oldtio) == -1) { /* save current port settings */
-      perror("tcgetattr");
-      exit(-1);
+    if(tcgetattr(fd, &oldtio) == -1) { /* save current port settings */
+        perror("tcgetattr");
+        exit(-1);
     }
 
-    bzero(&newtio, sizeof(newtio));/* preenche a struct newtio com zeros*/
+    bzero(&newtio, sizeof(newtio)); /* preenche a struct newtio com zeros*/
     newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
     newtio.c_iflag = IGNPAR;
     newtio.c_oflag = 0;
@@ -51,32 +52,27 @@ int main(int argc, char** argv)  /* fazer write da serial port */
     /* set input mode (non-canonical, no echo,...) */
     newtio.c_lflag = 0;
 
-    newtio.c_cc[VTIME]    = 1;   /* inter-character timer unused */
-    newtio.c_cc[VMIN]     = 5;   /* blocking read until 5 chars received */
+    newtio.c_cc[VTIME] = 1; /* inter-character timer unused */
+    newtio.c_cc[VMIN] = 5;  /* blocking read until 5 chars received */
 
-
-
-  /* FEITO
+    /* FEITO
     VTIME e VMIN devem ser alterados de forma a proteger com um temporizador a 
     leitura do(s) pr�ximo(s) caracter(es)
   */
 
-
-
     tcflush(fd, TCIOFLUSH);
 
-    if ( tcsetattr(fd,TCSANOW,&newtio) == -1) {
-      perror("tcsetattr");
-      exit(-1);
+    if(tcsetattr(fd, TCSANOW, &newtio) == -1) {
+        perror("tcsetattr");
+        exit(-1);
     }
 
     printf("New termios structure set\n");
 
+    printf("Escrever o que se quer enviar para o outro computador:");
+    gets(buf);
 
-	printf("Escrever o que se quer enviar para o outro computador:");
-	gets(buf);
-	
-	/*
+    /*
     for (i = 0; i < 255; i++) {  queremos que o buf leia da consola o que � suposto enviar
 		
 		
@@ -84,29 +80,22 @@ int main(int argc, char** argv)  /* fazer write da serial port */
 		
      buf[i] = 'a'; what is this aaaaaaaaaaaa,preencheu-se o buf para depois enviar
     }   */
-    
+
     /*testing*/
     buf[25] = '\n';
-    
-    res = write(fd,buf,255);   
-    printf("%d bytes written\n", res);
- 
 
-  /* 
+    res = write(fd, buf, 255);
+    printf("%d bytes written\n", res);
+
+    /* 
     O ciclo FOR e as instru��es seguintes devem ser alterados de modo a respeitar 
     o indicado no gui�o 
   */
 
-
-
-   
-    if ( tcsetattr(fd,TCSANOW,&oldtio) == -1) {
-      perror("tcsetattr");
-      exit(-1);
+    if(tcsetattr(fd, TCSANOW, &oldtio) == -1) {
+        perror("tcsetattr");
+        exit(-1);
     }
-
-
-
 
     close(fd);
     return 0;
