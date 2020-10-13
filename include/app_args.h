719380cd637ca14b8586e7981210b60865b1f2e1
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2020-10-13 17:16:32 +0100

    Documented args

--- include/app_args.h
+++ include/app_args.h
@@ -12,6 +12,18 @@
 
 #include "ll.h"
 
+/**
+ * @ingroup app_args
+ * @brief Parse app arguments
+ * 
+ * @param argc      Argument count
+ * @param argv      Argument vector
+ * @param com       Pointer to int to which COM will be stored
+ * @param status    Status (TRANSMITTER or RECEIVER)
+ * @param file_path Pointer to string where file path to transfer will be stored.
+ *                  If status is RECEIVER, file_path is not used and should be set to NULL
+ * @return int      0 if success, 1 otherwise
+ */
 int app_parse_args(int argc, char *argv[], int *com, ll_status_t status, char **file_path)
     __attribute__((warn_unused_result));
 
