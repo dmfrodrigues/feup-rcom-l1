c4597acc789dc9c8b616146086318206253e7b44
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2020-10-18 19:24:59 +0100

    Reduced pause after transmitter

--- stats/stats_transmitter.c
+++ stats/stats_transmitter.c
@@ -192,7 +192,7 @@ int main(int argc, char *argv[]){
         fprintf(stderr, "%8lu %6.2f%% (%6.1fs; %6.1fs left)\n",
             T, (100.0*idx_line)/num_lines, total_time, left_time);
 
-        usleep(100000);
+        usleep(10000);
     }
 
     return 0;
